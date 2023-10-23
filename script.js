ebae848f7f74d8b49f2fbfafa989d7072f6c44c0
3xion <88562716+3xionDev@users.noreply.github.com>
2023-10-23 10:49:15 -0400

    source site files

---
+++ script.js
@@ -0,0 +1,15 @@
+function replclicker() {
+  window.open("https://repl-clicker.3xion.repl.co")
+}
+function replclickertesting() {
+  window.open("https://repl-clicker-testing-11.3xion.repl.co")
+}
+function fightjs() {
+  window.open("https://fightjs.3xion.repl.co")
+}
+function backrooms() {
+  window.open("https://the-backrooms-v13.3xion.repl.co")
+}
+function chatti() {
+  window.open("https://chatti.3xion.repl.co")
+}
